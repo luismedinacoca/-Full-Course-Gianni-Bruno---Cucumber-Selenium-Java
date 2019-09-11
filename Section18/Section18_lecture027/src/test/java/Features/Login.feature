8b91a81a64b458f805ef9989e2eb97e9c3a75a0e
Louis <luismedinacoca@gmail.com>
2019-09-11 17:58:44 -0300

    first commit

---
+++ Section18/Section18_lecture027/src/test/java/Features/Login.feature
@@ -0,0 +1,9 @@
+Feature: Login into account
+  Existing user should be able to login to account using correct credentials
+  Scenario: Login into account with correct credentials
+    Given user navigate to stackoverflow website
+    And User click on login button
+    And User enters a valid username
+    And User enters a valid password
+    When User clicks on the login button
+    Then User should be taken to the successful login page
---
+++ Section18/Section18_lecture027/src/test/java/Step_Definitions/LoginSteps.java
@@ -0,0 +1,44 @@
+package Step_Definitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginSteps {
+    @Given("^user navigate to stackoverflow website$")
+    public void user_navigate_to_stackoverflow_website() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^User click on login button$")
+    public void user_click_on_login_button() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^User enters a valid username$")
+    public void user_enters_a_valid_username() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^User enters a valid password$")
+    public void user_enters_a_valid_password() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User clicks on the login button$")
+    public void user_clicks_on_the_login_button() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^User should be taken to the successful login page$")
+    public void user_should_be_taken_to_the_successful_login_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
